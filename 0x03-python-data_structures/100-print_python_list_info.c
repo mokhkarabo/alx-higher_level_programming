99e3fa3c2e2396b61d3de9045bb711ad6809d55a
mokhkarabo <mokherere.karabo@gmail.com>
2024-01-08 18:09:57 -0800

    Linked list palindrome

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <python.h>
+#include <object.h>
+#include <listobject.h>
 /**
  * print_python_list_info -  function that prints some basic
  *							info about Python lists
