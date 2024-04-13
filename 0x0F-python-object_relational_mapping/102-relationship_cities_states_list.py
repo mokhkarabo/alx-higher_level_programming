fba0cc1cd2728944e0ad1bcd6248801369e5badc
mokhkarabo <mokherere.karabo@gmail.com>
2024-04-13 04:45:52 -0700

    relations

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 import sys
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
 
+Base = declarative_base()
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+    state = relationship("State")
+
+def list_cities(username, password, dbname):
+    """Connect to the database and list all cities with their states."""
+    engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@localhost:3306/{dbname}')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        for city_ins in instance.cities:
-            print(city_ins.id, city_ins.name, sep=": ", end="")
-            print(" -> " + instance.name)
 
+    # Querying cities and joining with states, sorting by city id
+    cities = session.query(City).join(State).order_by(City.id).all()
+    
+    for city in cities:
+        print(f"{city.id}: {city.name} -> {city.state.name}")
+
+    session.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
+        list_cities(username, password, dbname)
+    else:
+        print("Usage: script.py <mysql username> <mysql password> <database name>")
