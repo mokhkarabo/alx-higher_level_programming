f5da1d34233ca94f27fd54e9bcd8011e52189ffd
mokhkarabo <mokherere.karabo@gmail.com>
2024-04-25 16:50:11 -0700

    retry

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Make a request to 0.0.0.0:5000/catch_me with curl, send POST request with empty data
+# Make a request to 0.0.0.0:5000/catch_me with curl, sending POST request with empty data
 response=$(curl -s -X POST http://0.0.0.0:5000/catch_me --data "")
 
-# Print the response body
-echo "$response" | grep -o "You got me!"
+# Check if the response contains the message "You got me!"
+if [[ $response == *"You got me!"* ]]; then
+    # If it does, exit successfully
+    exit 0
+else
+    # If it doesn't, exit with an error
+    exit 1
+fi
